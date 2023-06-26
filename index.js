46cb15229c98cf3d39b6fa01affd6cfb5b3d7370
Edmond Serato <alphashadow0418@gmail.com>
2023-06-27 01:31:01 +0800

    working on login functionality

--- index.js
+++ index.js
@@ -23,10 +23,54 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 //form
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
+const formButton = document.getElementById('formButton');
+const myForm = document.getElementById('myForm');
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+// Add click event listener to the button
+formButton.addEventListener('click', function() {
+  if (myForm.style.display === 'none') {
+    myForm.style.display = 'block';
+  } else {
+    myForm.style.display = 'none';
+  }
+});
+
+//form funtionality
+let myWarning = document.getElementById("head");
+let children = myWarning.getElementsByClassName('myChildren');
+let myPrompt;
+function logIn(){
+  let defaultUserName = "admin";
+  let defaultPassword = "password";
+  let myUserName = document.getElementById("userName").value;
+  let myPassword = document.getElementById("password").value;
+
+
+  if (myUserName == defaultUserName && myPassword == defaultPassword){
+    remove();
+    myPrompt = document.createElement("div");
+    myPrompt.classList = "myChildren success";
+    myPrompt.textContent = "Succesfully Login";
+    myWarning.appendChild(myPrompt);
+  }
+  else if (myUserName == "" && myPassword == ""){
+    remove();
+    myPrompt = document.createElement("div");
+    myPrompt.classList = "myChildren warningEmpty";
+    myPrompt.textContent = "please input username and password";
+    myWarning.appendChild(myPrompt);
+  }
+  else{
+    remove();
+    myPrompt = document.createElement("div");
+    myPrompt.classList = "myChildren warningWrongPassword";
+    myPrompt.textContent = "invalid username or password";
+    myWarning.appendChild(myPrompt);
+  }
+  
+}
+function remove(){
+  while (children.length > 0) {
+    children[0].parentNode.removeChild(children[0]);
+  }  
 }
