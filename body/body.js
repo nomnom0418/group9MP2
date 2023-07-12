260c126b07a269f89149c1c00a759a8dd89510cf
Edmond Serato <alphashadow0418@gmail.com>
2023-07-12 11:29:49 +0800

    pagination adden in my homepage

--- body/body.js
+++ body/body.js
@@ -1,51 +1,93 @@
-createChild(50);
+let addChildNumber=6;
+let childNumber=6;
+console.log(addClick(addChildNumber,childNumber))
+function addClick(num,add){
+  $('.moreContainer').click(function(){
+    num=num+add;
+    createChild(num);
+  })
+  return num;
+}
+
+
+//more_(addChildNumber);
+
+createChild(childNumber);
 //this is for creating children in the body
-function createChild(createdPost){
+function createChild(passNumber){
+  console.log(passNumber)
   let blogParentPost=$('.blogsParent');
   let picChild="";
-  for(let i=1; i<=createdPost; i++){
-  picChild = picChild+ '<div class="blogContainer'+i+'"><div class="blogPicture'+i+'">this is blog picture</div><div class="blogFooter'+i+'"><div class="creatorPic'+i+'"> creator </div><div class="blogInfo'+i+'"> blog information</div></div></div>';
+  for(let i=1; i<=passNumber; i++){
+  picChild = picChild+ '<div class="blogContainer blogContainer'+i+'"><div class="blogPicture blogPicture'+i+'"></div><div class="blogFooter blogFooter'+i+'"><div class="creatorPic creatorPic'+i+'"></div><div class="blogInfo blogInfo'+i+'"></div>  </div></div>';
   blogParentPost.html(picChild);
   }
-  let blogCon=$('.blogContainer');
-  blogCon.css({
-    'display':'grid',
-    'gridTemplateRows':'190px 60px',
-    'height':'250px',
-    'width':'320px',
-    'backgroundColor':'white',
-    'margin':'5px 5px',
-    'border-radius':'5px',
-  });
-  $('.blogPicture').css({
-    'backgroundColor':'lightblue',
-  });
-  $('.blogFooter').css({
-    'display':'grid',
-    
-    'color':'white'
-  });
-  $('.creatorPic').css({
-    'cursor':'pointer',
-    'height':'40px',
-    'width':'40px',
-  });
-  $('.blogInfo').css({
-    'cursor':'pointer'
-  });
-  
-  
+
  //this is for body height 
-  let blogsContainerHeight = ((createdPost/3)*305);
-  $('.blogsContainer').css({
-    'height': blogsContainerHeight +"px"
-  });
-  let parentBlogsHeight = blogsContainerHeight-4;
-  $('.blogsParent').css({
-    'height': parentBlogsHeight +"px"
-  });
-  let bodyHeight = blogsContainerHeight +200;
-  $('.mainContainer').css({
-    'height': bodyHeight +"px"
-  });
+  let blogsContainerHeight=0;
+  let parentBlogsHeight =0;
+  let bodyHeight =0;
+  let screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  let screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  if (screenWidth > 1230){
+    blogsContainerHeight = ((passNumber/3)*280);
+    $('.blogsContainer').css({
+      'height': blogsContainerHeight +"px"
+    });
+    parentBlogsHeight = blogsContainerHeight-4;
+    $('.blogsParent').css({
+      'height': parentBlogsHeight +"px"
+    });
+    bodyHeight = blogsContainerHeight +200;
+    $('.mainContainer').css({
+      'height': bodyHeight +"px"
+    });
+  }else if (screenWidth==912 && screenHeight==1368) {
+    blogsContainerHeight = ((passNumber/2)*280);
+    $('.blogsContainer').css({
+      'height': blogsContainerHeight +"px"
+    });
+    parentBlogsHeight = blogsContainerHeight-4;
+    $('.blogsParent').css({
+      'height': parentBlogsHeight +"px"
+    });
+    bodyHeight = blogsContainerHeight +495;
+    $('.mainContainer').css({
+      'height': bodyHeight +"px"
+    });
+
+    
+  }else if (screenWidth<=1230 && screenWidth>=824) {
+    blogsContainerHeight = ((passNumber/2)*280);
+    $('.blogsContainer').css({
+      'height': blogsContainerHeight +"px"
+    });
+    parentBlogsHeight = blogsContainerHeight-4;
+    $('.blogsParent').css({
+      'height': parentBlogsHeight +"px"
+    });
+    bodyHeight = blogsContainerHeight +200;
+    $('.mainContainer').css({
+      'height': bodyHeight +"px"
+    });
+
+    } else{
+    blogsContainerHeight = (passNumber*280);
+    $('.blogsContainer').css({
+      'height': blogsContainerHeight +"px"
+    });
+    parentBlogsHeight = blogsContainerHeight-4;
+    $('.blogsParent').css({
+      'height': parentBlogsHeight +"px"
+    });
+    bodyHeight = blogsContainerHeight +200;
+    $('.mainContainer').css({
+      'height': bodyHeight +"px"
+    });
+  }
 }
+
+
+
+
+
